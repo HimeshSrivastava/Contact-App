9e092d3b214e0d386abda3a288c9ef74399a1ddb
HimeshSrivastava <153629881+HimeshSrivastava@users.noreply.github.com>
2024-10-28 08:26:12 +0530

    updated app

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,19 @@
-# Use the official Python image as a base
+
 FROM python:3.9-slim
 
-# Set the working directory in the container
 WORKDIR /app
 
-# Copy the requirements file to the container
 COPY requirements.txt .
 
-# Install dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the rest of the application code
+
 COPY . .
 
-# Expose the port Flask will run on
 EXPOSE 5000
 
-# Set environment variables (optional)
 ENV FLASK_APP=main.py
 ENV FLASK_RUN_HOST=0.0.0.0
 ENV FLASK_ENV=development
 
-# Run the Flask application
 CMD ["flask", "run"]
--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-from .config import Config  # Ensure config.py is in the same directory
-from .routes import user_bp   # Ensure routes.py is in the same directory
+from .config import Config  
+from .routes import user_bp   
 
 mongo = PyMongo()
 
@@ -9,10 +9,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
-    # Initialize PyMongo
     mongo.init_app(app)
     
-    # Register Blueprints
     app.register_blueprint(user_bp, url_prefix="/api/users")
     
     return app
